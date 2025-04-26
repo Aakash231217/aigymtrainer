8910eed29a9191b72204242d65b694437575632c
Aakash Singh <92391710+Aakash231217@users.noreply.github.com>
2025-04-27 00:25:46 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,7 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   // Define your static routes
   const staticRoutes = [
     '/',
-    ''.
+    '',
     '/generate-program',
     '/profile',
     // Add all your static routes
